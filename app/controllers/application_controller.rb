42e233dda3f53558965a6af2c62b6889b147c6de
sakakidai <sd.lybd1016@gmail.com>
2021-05-01 13:16:06 +0900

    create registration devise api

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,5 @@
 class ApplicationController < ActionController::Base
+  include DeviseTokenAuth::Concerns::SetUserByToken
+  # テスト用トークンが一致しなくても例外が発生しない
+  protect_from_forgery
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,12 @@
 Rails.application.routes.draw do
   root to: 'home#index'
+  namespace :api, defaults: { format: :json } do
+    scope :v1 do
+      mount_devise_token_auth_for 'User', at: 'auth', controllers: {
+        registrations: 'api/v1/auth/registrations'
+      }
+    end
+  end
   if Rails.env.development?
     mount LetterOpenerWeb::Engine, at: '/letter_opener'
   end
