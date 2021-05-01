252dd25a9045ce64b19405af4ba6a4469a4ce9a6
sakakidai <sd.lybd1016@gmail.com>
2021-05-01 16:52:17 +0900

    Add vuetify in vue

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -15,17 +15,21 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 // import store from '../store'
 import router from '../routes'
+import Vuetify from "vuetify"
 import App from '../app'
 
+import "vuetify/dist/vuetify.min.css"
 import '../assets/css/application'
 
 // Vue.use(Vuex)
+Vue.use(Vuetify)
 Vue.use(VueRouter)
 
 document.addEventListener('DOMContentLoaded', () => {
   const app = new Vue({
     // store: new Vuex.Store(store),
     router,
+    vuetify: new Vuetify(),
     render: h => h(App)
   }).$mount()
   document.body.appendChild(app.$el)
